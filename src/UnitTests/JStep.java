1799d07fc5f73bf4d4136dd86cf4d9b0f8151e21
staywarde <kim@searchwar.dk>
2015-05-20 10:48:08 +0200

    Added some negative tests and a test to find item in database

--- src/UnitTests/JStep.java
+++ src/UnitTests/JStep.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.awt.List;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import org.junit.Test;
 
 import CtrLayer.StepCtr;
@@ -19,15 +20,54 @@ import ModelLayer.Step;
 public class JStep {
 
 	@Test
-	public void testStepDB() throws SQLException {
+	public void positiveTest_FindOneOrMore() throws Exception, SQLException {
 		StepCtr sctr = new StepCtr();
-		ArrayList<Step> steps = (ArrayList<Step>)sctr.findNextSteps(1);
-
+		ArrayList<Step> steps = (ArrayList<Step>)sctr.findNextSteps(2);
+		
+		Boolean foundStepItem = false;
+		// Print steps in console
+		for (Step s : steps) {
+			if (s.getName().equals("Pizza skal hentets")) {
+				foundStepItem = true;
+			}
+			
+			System.out.println(s.getName());
+		}
+		
+		// Validate an item in database
+		if (!foundStepItem) {
+			throw new Exception("'Pizza skal hentets' item is not found");
+		}
+		
+		// Validate there is one or more items
+		assertTrue("Should contain one or more Step", steps.size() > 0);
+	}
+	
+	@Test
+	public void negativeTest_NoSteps() throws Exception, SQLException {
+		StepCtr sctr = new StepCtr();
+		ArrayList<Step> steps = (ArrayList<Step>)sctr.findNextSteps(2222);
+		
+		// Print steps in console
 		for (Step s : steps) {
 			System.out.println(s.getName());
 		}
-
-		assertTrue("Should contain one or more Step", steps.size() > 0);
+		
+		// Validate there is one or more items
+		assertTrue("Should not contain one or more Step", steps.size() == 0);
 	}
-
+	
+	@Test
+	public void negativeTest_NoSteps_WithNegativeNumber() throws Exception, SQLException {
+		StepCtr sctr = new StepCtr();
+		ArrayList<Step> steps = (ArrayList<Step>)sctr.findNextSteps(-1);
+		
+		// Print steps in console
+		for (Step s : steps) {
+			System.out.println(s.getName());
+		}
+		
+		// Validate there is one or more items
+		assertTrue("Should not contain one or more Step", steps.size() == 0);
+	}
 }
