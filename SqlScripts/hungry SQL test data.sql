4849d8aec9c574d674cb09fb7708520655d4f31a
staywarde <kim@searchwar.dk>
2015-05-22 09:57:40 +0200

    SQL Fixs

--- SqlScripts/hungry SQL test data.sql
+++ SqlScripts/hungry SQL test data.sql
@@ -15,50 +15,50 @@ INSERT INTO [Town] (zip, name) VALUES(9400, 'Nørresundby');
 INSERT INTO [Town] (zip, name) VALUES(9000, 'Aalborg');
 
 INSERT INTO [Person] (name, zip, street, phone) VALUES('Jens Henrik', 9400, 'Møllevej 4, 1', '+45 25363897');
-SET @PersonID = IDENT_CURRENT( 'Person' );
+SET @PersonID = scope_identity();
 INSERT INTO [Person] (name, zip, street, phone) VALUES('Per Jensen', 9000, 'vesterbro 2', '+45 54545555');
-SET @EmployeePersonID = IDENT_CURRENT( 'Person' );
+SET @EmployeePersonID = scope_identity();
 
 INSERT INTO [Customer] (person_id, email) VALUES(@PersonID, 'kim@jenshenrik.dk');
 
 
 INSERT INTO [Restaurant] (name, street, zip, phone, email, website) VALUES('Jørgen Pizaa A/S', 'vesterbro 55', 9000, '+45 22222222', 'kontakt@joergen.dk', 'http://www.joergenpizza.dk');
-SET @RestaurantID = IDENT_CURRENT( 'Restaurant' );
+SET @RestaurantID = scope_identity();
 
 INSERT INTO Employee (person_id, position, rest_id, employee_no) VALUES(@EmployeePersonID, 'Pizza-bager', @RestaurantID, 1);
 
 INSERT INTO [Product] (name, price, rest_id) VALUES('Pizza', '55.0', @RestaurantID);
-SET @ProductID = IDENT_CURRENT( 'Product' );
+SET @ProductID = scope_identity();
 
 INSERT INTO [Order] ([date], rest_id, customer_id) VALUES(GETDATE(), @RestaurantID, @PersonID);
-SET @OrderID = IDENT_CURRENT( 'Order' );
+SET @OrderID = scope_identity();
 
 INSERT INTO [PartOrder] (amount, product_id, order_id) VALUES('2', @ProductID, @OrderID);
 
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Bager pizza', 'Laver pizza til ovnen', @RestaurantID, 'false');
-SET @stepBagerID = IDENT_CURRENT( 'Step' );
+SET @stepBagerID = scope_identity();
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Pizza i ovnen', 'Pizza er i ovnen', @RestaurantID, 'false');
-SET @stepPizzaIOvnID = IDENT_CURRENT( 'Step' );
+SET @stepPizzaIOvnID = scope_identity();
 
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepBagerID, @stepPizzaIOvnID);
 
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Pizza skal hentet', 'pizza skal hentet', @RestaurantID, 'false');
-SET @stepPizzaHentet = IDENT_CURRENT( 'Step' );
+SET @stepPizzaHentet = scope_identity();
 
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepPizzaIOvnID, @stepPizzaHentet);
 
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Pizza skal afleveres', 'Pizza skal afleveres', @RestaurantID, 'false');
-SET @stepPizzaAfleveres = IDENT_CURRENT( 'Step' );
+SET @stepPizzaAfleveres = scope_identity();
 
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepPizzaIOvnID, @stepPizzaAfleveres);
 
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Pizza er på vej', 'Pizza er afleveret', @RestaurantID, 'false');
-SET @stepPizzaErPåVej = IDENT_CURRENT( 'Step' );
+SET @stepPizzaErPåVej = scope_identity();
 
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepPizzaAfleveres, @stepPizzaErPåVej);
 
 INSERT INTO [Step] (name, description, rest_id, is_last_step) VALUES('Pizza er afleveret eller hentet', 'Pizza er afleveret eller hentet', @RestaurantID, 'false');
-SET @stepPizzaErAfleveret = IDENT_CURRENT( 'Step' );
+SET @stepPizzaErAfleveret = scope_identity();
 
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepPizzaErPåVej, @stepPizzaErAfleveret);
 INSERT INTO [StepRelation] (step_id, nextstep_id) VALUES(@stepPizzaHentet, @stepPizzaErAfleveret);
