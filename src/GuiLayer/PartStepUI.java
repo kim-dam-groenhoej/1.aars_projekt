5067f59cccfbe6639a18fa143d2e81c48236a87e
staywarde <kim@searchwar.dk>
2015-05-28 09:51:03 +0200

    Fixed gui rows

--- src/GuiLayer/PartStepUI.java
+++ src/GuiLayer/PartStepUI.java
@@ -89,12 +89,9 @@ public class PartStepUI extends JFrame {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(null);
 		scrollPane.setViewportView(panel_1);
+		panel_1.setLayout(new MigLayout("", "[480px]", "[200px]"));
 		
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		
-		createOrderItems(gl_panel_1);
-		
-		panel_1.setLayout(gl_panel_1);
+		createOrderItems(panel_1);
 		
 		JScrollBar scrollBar = new JScrollBar();
 		scrollBar.setBounds(0, 0, 200, 50);
@@ -104,9 +101,29 @@ public class PartStepUI extends JFrame {
 		this.setSize(new Dimension(1024, 768));
 	}
 	
-	private void createOrderItems(GroupLayout groupL)
+	private void createOrderItems(JPanel panel1)
 	{
-		groupL.setHorizontalGroup(groupL.createParallelGroup(Alignment.LEADING));
+		for (int i = 0; i<10;i++) {
+			JPanel panel_7 = new JPanel();
+			panel_7.setLayout(null);
+			
+			// add panel to rows
+			panel1.add(panel_7, "cell 0 " + i + ",grow");
+
+			// items for panel
+			JLabel lblNewLabel_2 = new JLabel("New label");
+			lblNewLabel_2.setBounds(10, 11, 46, 14);
+			panel_7.add(lblNewLabel_2);
+			
+			JScrollPane scrollPane_1 = new JScrollPane();
+			scrollPane_1.setBounds(253, 11, 191, 97);
+			panel_7.add(scrollPane_1);
+			
+			JPanel panel_4 = new JPanel();
+			scrollPane_1.setViewportView(panel_4);
+		}
+		
+		/*groupL.setHorizontalGroup(groupL.createParallelGroup(Alignment.LEADING));
 		groupL.setVerticalGroup(groupL.createParallelGroup(Alignment.LEADING));
 		
 		SequentialGroup ggg = groupL.createSequentialGroup();
@@ -115,14 +132,13 @@ public class PartStepUI extends JFrame {
 		SequentialGroup aaa = groupL.createSequentialGroup();
 		aaa = aaa.addContainerGap();
 		
-		//for (int i = 0; i<10;i++) {
+		for (int i = 0; i<10;i++) {
 			
 			JPanel panel_3 = new JPanel();
-			JPanel panel_3_1 = new JPanel();
 			
-			ggg = ggg.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 454, Short.MAX_VALUE).addComponent(panel_3_1, GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE).addContainerGap();
+			ggg = ggg.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 454, GroupLayout.PREFERRED_SIZE);
 		
-			aaa = aaa.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE).addComponent(panel_3_1, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE).addContainerGap(566, Short.MAX_VALUE);
+			aaa = aaa.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 105 + (11 * i), GroupLayout.PREFERRED_SIZE).addPreferredGap(ComponentPlacement.UNRELATED);
 			
 
 			
@@ -136,9 +152,12 @@ public class PartStepUI extends JFrame {
 			
 			JPanel panel_4 = new JPanel();
 			scrollPane_1.setViewportView(panel_4);
-		//}
+		}
+		
+		ggg = ggg.addContainerGap();
+		aaa = aaa.addContainerGap(430, Short.MAX_VALUE);
 		
 		groupL.setHorizontalGroup(groupL.createParallelGroup(Alignment.LEADING).addGroup(ggg));
-		groupL.setVerticalGroup(groupL.createParallelGroup(Alignment.LEADING).addGroup(aaa));
+		groupL.setVerticalGroup(groupL.createParallelGroup(Alignment.LEADING).addGroup(aaa));*/
 	}
 }
